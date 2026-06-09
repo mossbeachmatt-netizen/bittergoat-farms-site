c24c1a070b0a9d67b87c9dca82d4b382ed10931b
Matt <raidertrader434@gmail.com>
2026-06-09 15:52:17 -0700

    Rebuild with FishAnimator Resources fix

--- public/games/dragon-palace/Build/DragonPalace.loader.js
+++ public/games/dragon-palace/Build/DragonPalace.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58456866,
+    wasmFileSize: 58457780,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
